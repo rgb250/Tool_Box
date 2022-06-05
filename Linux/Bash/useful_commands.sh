fa13ee343e3c2de03cdb19da241c396cd035e156
rgb250 <regis.gbenou@outlook.fr>
2022-06-05 11:53:00 +0200

    Folder Adaptation: Refactor Windows and Bash.

--- Linux/Bash/useful_commands.sh
+++ Linux/Bash/useful_commands.sh
@@ -4,4 +4,6 @@ free -m # display all the information in MBs
 df -h # disk filename command, with the human readable option (adapt the figures of each filesystem)
 du -ch # to see the size of each elements in a given repository 
 
+# SEARCH INSIDE FILES
 grep --exclude=\*.{pyc,log} -rl './' -e ".*10\.214\.82\.92.*" # find in the current directory and its subdirectorires all the files containing the string "10.214.82.92" inside a 
+grep --include=*.tex -lri monte ./* # find all files containing at least one occurence of "monte" inside it 
