13fe5cfa7c951f46a16d8cd0070ab00bd58db953
rgb250 <regis.gbenou@outlook.fr>
2022-09-17 00:34:23 +0200

    Data Science: Added some information related to Bayesian approach

--- Linux/Bash/useful_commands.sh
+++ Linux/Bash/useful_commands.sh
@@ -7,3 +7,11 @@ du -ch # to see the size of each elements in a given repository
 # SEARCH INSIDE FILES
 grep --exclude=\*.{pyc,log} -rl './' -e ".*10\.214\.82\.92.*" # find in the current directory and its subdirectorires all the files containing the string "10.214.82.92" inside a 
 grep --include=*.tex -lri monte ./* # find all files containing at least one occurence of "monte" inside it 
+
+# Run process in the background --> use ampersand symbol (&)
+vlc > /dev/null 2>&1 &  # >/dev/null 2>&1 means stdout --> /dev/null and stderr --> stdout
+jobs -1  # displays the status of all stopped and background jobs in the current session
+fg  # bring a process to the foreground
+bg  # bring a process to the background
+nohup vlc &  # nohup ignore all SIGHUP (hangup) signals, sent when terminal is closed
+
